a205f8556970268c77b409616aa8517b5d1a54db
Gabriela Dellamora Paim <gabriela@dellamora.dev>
2025-06-19 22:03:27 -0300

    Update main and model

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
+import os
 import json
-from genetic_algorithm import GeneticAlgorithm
-from genetic_algorithm import FitnessEvaluator
+from genetic_algorithm import GeneticAlgorithm, FitnessEvaluator
+from model import MultilayerPerceptron, Minimax
 
-# Models
-from model import MultilayerPerceptron
-from model import Minimax
+def train(learner: MultilayerPerceptron, trainer: Minimax, population_size: int, pipeline: list[str],
+          max_iter: int = 100, threshold: float = 500, learning_rate: float = 0.1,
+          verbose: bool = False) -> MultilayerPerceptron:
 
-
-def train(learner: MultilayerPerceptron, trainer: Minimax, population_size:int, pipeline=list[str], max_iter:int=100, threshold:float=0.95, learning_rate:float=0.1, verbose:bool=False) -> MultilayerPerceptron:
-    print(f"Partidas : Easy={pipeline.count('easy')} : Medium={pipeline.count('medium')} : Hard={pipeline.count('hard')}")
+    print(f"Main : Pipeline de dificuldades: Easy={pipeline.count('easy')} | Medium={pipeline.count('medium')} | Hard={pipeline.count('hard')}")
 
     training = GeneticAlgorithm(
-        pop_size         =population_size,
-        chromosome_size  =learner.count_weights(),
-        fitness_function =FitnessEvaluator(learner, trainer, pipeline, False),
-        max_iter         =max_iter,
-        learning_rate    =learning_rate,
-        verbose          =verbose
+        pop_size=population_size,
+        chromosome_size=learner.count_weights(),
+        fitness_function=FitnessEvaluator(learner, trainer, pipeline, verbose),
+        max_iter=max_iter,
+        learning_rate=learning_rate,
+        verbose=verbose
     )
 
     best_chromosomes = training.run(threshold=threshold)
@@ -24,27 +23,39 @@ def train(learner: MultilayerPerceptron, trainer: Minimax, population_size:int, 
     return learner
 
 if __name__ == '__main__':
+    # Garante que a pasta existe
+    os.makedirs('output', exist_ok=True)
+
+    # Define parâmetros do treinamento
     PIPELINE = (
-    4 * ['medium'] +
-    8 * ['hard']
+        7 * ['medium'] +
+        10 * ['hard']
     )
 
-    TOPOLOGY=[9, 9, 9]
+    TOPOLOGY = [9, 18, 9]
 
     VERBOSE = False
 
     model = MultilayerPerceptron(TOPOLOGY)
     minimax = Minimax()
 
-    train(
+    # Treina o modelo
+    model = train(
         learner=model,
         trainer=minimax,
-        population_size=1000,
+        population_size=4000,
         pipeline=PIPELINE,
-        max_iter=400,
-        learning_rate=0.5,
+        max_iter=250,
+        learning_rate=0.4,
+        threshold=1000,
         verbose=VERBOSE,
-        )
+    )
 
+    # Salva o modelo
     with open('output/model.json', 'w') as f:
-        f.write(str(model.to_json()))
+        json.dump(model.to_json(), f)
+
+    # Testa o modelo após o treinamento
+    print("Main : Avaliação contra o Minimax:")
+    model.set_verbose(True)
+    FitnessEvaluator.test_model(model, minimax, rounds=50)
